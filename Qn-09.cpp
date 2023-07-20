8450992b31c087ee166e02e3e239841680a159d6
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-20 14:04:11 +0530

    Update Qn-09.cpp

--- Qn-09.cpp
+++ Qn-09.cpp
@@ -20,7 +20,7 @@ class mystring
         void setString()
         {
             cout<<"Enter a string : ";
-            cin.getline(str,100) ;
+            cin.getline(str,100); 
             cout<<endl ;
         }
         void showData()
@@ -28,32 +28,28 @@ class mystring
             cout<<str<<endl ;
         }
 
-        mystring operator ! ()
+        void operator ! ()
         {
-            mystring temp ;
             int i ;
-            for(i=0 ; str[i] ; i++)
+            for(i=0 ; str[i]!='\0' ; i++)
             {
-                if ( str[i] >=97 && str[i]<=122)
-                    temp.str[i] = str[i] - 32 ;
+                if (str[i] >=65 && str[i]<=96)
+                    str[i] = str[i] + 32 ;
 
-                else if (str[i] >=65 && str[i]<=96)
-                    temp.str[i] = str[i] + 32 ;
-            }   
-            return temp ;
+                else if ( str[i] >=97 && str[i]<=122)
+                    str[i] = str[i] - 32 ;
+            }
+            cout<<"Reverse string is : "<<str <<endl;   
         }
 };
 
 int main()
 {
-    mystring s1,s2 ;
+    mystring s1 ;
     s1.setString() ;
     cout<<"My String is : ";
     s1.showData() ;
 
-    s2 = !s1 ;
-
-    s2.showData() ;
-
-
+    !s1 ;
+    return 0 ;
 }
